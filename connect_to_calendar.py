444e1e3eca414151f6a60dfa5bbaead262699aea
Blazhevskiy <ghostf987@gmail.com>
2020-01-16 14:13:25 +0200

    Calendar (#5)
    * Test Commit
    
    * True test commit
    
    * Add connection to calendar
    
    * Add create event base
    
    * Add connection to calendar
    
    * Update main.py
    
    * Update main.py
    
    * Update .gitignore
    
    * Update connect_to_calendar.py
    
    * Update connect_to_calendar.py
    
    * Update connect_to_calendar.py
    
    * Update connect_to_calendar.py

---
+++ connect_to_calendar.py
@@ -0,0 +1,75 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+creds = None
+
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+
+service = build('calendar', 'v3', credentials=creds)
+
+
+#Get last 10 events in calendar 
+def get_last_10_events():
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        return start, event['summary']
+
+#Create event 
+def create_event():
+    event = {
+        'summary': 'Appointment',
+        'location': 'Odrex',
+        'description': 'Reception bot for making appointments.',
+        'start': {
+            'dateTime': '2020-01-14T09:00:00-07:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'end': {
+            'dateTime': '2020-01-14T17:00:00-07:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'attendees': [
+            {'email': 'ghostf987@gmail.com'},
+        ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+            {'method': 'email', 'minutes': 24 * 60},
+            {'method': 'popup', 'minutes': 10},
+            ],
+        },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print("Event created: {}".format(event.get('htmlLink')))
+ 
