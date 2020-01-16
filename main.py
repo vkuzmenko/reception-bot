52c5ab9dbbb4de005d10d9fd4cc832451c0b997d
Blazhevskiy <ghostf987@gmail.com>
2020-01-16 14:06:11 +0200

    First pull request #Fixes  (#2)
    * Test Commit
    
    * True test commit
    
    * Update main.py
    
    * Update main.py
    
    * Update .gitignore
    
    * Update main.py
    
    * Update README.md
    
    * Update .gitignore
    
    * Update main.py
    
    * Update main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import telebot
+from decouple import config
+
+bot = telebot.TeleBot(config('API_TOKEN'))
+
+start_keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+start_keyboard.row('Записаться')
+
+status = 0
+dates = []
+
+#Start chatting with bot 
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    status = 0
+    bot.send_message(message.chat.id, f"""Рады приветсвовать тебя в нашем боте.Он поможет тебе записаться к врачу хириргу-онкогинекологу Михаилу Александровичу""", reply_markup=start_keyboard)
+
+#Message handler
+@bot.message_handler(content_types=['text'])
+def resolve(message):
+    if message.text == 'Записаться':
+        bot.send_message(message.chat.id, "Введите дату начала записи")
+    else:
+        if dates == []:
+            status = 1 
+            check_in(message, status)
+        else:
+            status = 2
+            check_in(message, status)
+#Base functions 
+def check_in(message, status):
+    if status == 1:
+        print('Start date: {}'.format(message.text))
+    elif: status == 2: 
+        print('End date: {}'.format(message.text))
+
+bot.polling()
+
